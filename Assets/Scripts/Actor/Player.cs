de1febe038084507a24aaf44e2e30daaa7783c14
EXYS\hiroto.kaihatsu <noppo.kappa@gmail.com>
2018-05-10 19:05:34 +0900

    【プレイヤー】挙動追加

---
+++ Assets/Scripts/Actor/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    /// <summary>
+    /// プレイヤーの向き
+    /// </summary>
+    public enum Direction {RIGHT=-1,LEFT=1 }
+
+    private Vector3 move;
+    private Vector3 hitPos=Vector3.zero;
+
+    [SerializeField]
+    private GameObject bar;
+
+    [SerializeField]
+    private Direction m_currentDirection;
+
+    [SerializeField]
+    private Rigidbody2D m_rigidbody2D;
+
+    [SerializeField]
+    private SpriteRenderer m_spriteRenderer;
+
+    [SerializeField]
+    private float m_runSpeed;
+
+    private bool m_barflg;
+
+    private void Start(){
+
+        m_spriteRenderer.flipX = m_currentDirection == Direction.RIGHT;
+        move = new Vector3(m_runSpeed, 0, 0);
+    }
+
+    private void Run(){
+        if (Input.GetKey(KeyCode.RightArrow)){
+            m_spriteRenderer.flipX = m_currentDirection == Direction.RIGHT;
+            transform.position += move;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            m_spriteRenderer.flipX = m_currentDirection == Direction.LEFT;
+            transform.position -= move;
+        }
+    }
+
+    private void Slow(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+        if (Input.GetMouseButtonDown(0)){
+            if (hit.collider){
+                bar = Instantiate(bar, transform.position,Quaternion.identity);
+                hitPos = ray.origin;
+                m_barflg = true;
+            }
+        }
+
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        Run();
+    }
+}
---
+++ Assets/Scripts/SandBox/LerpTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpTest : MonoBehaviour {
+
+    [SerializeField, Range(0.0f, 10.0f)]
+    private float m_time = 0.0f;
+
+    [SerializeField]
+    private Vector3 m_targetPosition = Vector3.zero;
+
+    [SerializeField]
+    private Color m_startColor = Color.white;
+    [SerializeField]
+    private Color m_targetColor = Color.white;
+
+
+    private float m_startTime = 0.0f;
+    private Vector3 m_startPosition = Vector3.zero;
+
+
+    private Color CurrentColor
+    {
+        set
+        {
+            var render = GetComponent<Renderer>();
+            if (render == null)
+            {
+                return;
+            }
+
+            if (render.material == null)
+            {
+                return;
+            }
+
+            render.material.color = value;
+        }
+
+    }
+
+
+    void Start()
+    {
+        m_startTime = Time.time;
+        m_startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float timeStep = m_time > 0.0f ? (Time.time - m_startTime) / m_time : 1.0f;
+        timeStep = Mathf.Clamp01(timeStep);
+
+        transform.position = Vector3.Lerp(m_startPosition, m_targetPosition, timeStep);
+        CurrentColor = Color.Lerp(m_startColor, m_targetColor, timeStep);
+    }
+}
